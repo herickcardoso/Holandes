6273976280b6e4d797735adab50c9c5c0f82eced
herick <cardoso.herick@gmail.com>
2017-02-15 16:59:01 -0200

    BKP do estatico anterior II

---
+++ holandes/core/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.views.generic import CreateView, ListView
+from django.core.urlresolvers import reverse_lazy
+from django.core.paginator import Paginator
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
